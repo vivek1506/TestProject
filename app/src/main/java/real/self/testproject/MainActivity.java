859c51f9fac5c821e0e4e411a5c17d7d89831e91
Vivek kumar shukla <shuklavb209@gmail.com>
2022-03-02 16:38:55 +0530

    Friends

--- app/src/main/java/real/self/testproject/MainActivity.java
+++ app/src/main/java/real/self/testproject/MainActivity.java
@@ -13,10 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "first commit", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "first commit", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "first commit", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "first commit", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "first commit", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "first commit", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "first commit", Toast.LENGTH_SHORT).show();
+
     }
 }
